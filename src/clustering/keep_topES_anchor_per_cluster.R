ea97511fb10c14f36155f98d530c82cc26f12af2
Daniel Cotter <dcotter1@stanford.edu>
2024-10-10 11:11:30 -0700

    update .gitignore and track clustering file

---
+++ src/clustering/keep_topES_anchor_per_cluster.R
@@ -0,0 +1,110 @@
+# reorder_anchor_clusters.R
+# Daniel Cotter
+# 2024-09-12
+
+# This script takes in an anchor cluster file and a splash stats file and smartly 
+# reorders the anchor clusters (both the clusters and the anchors within the clusters)
+# based on the effect size and the number of nonzero samples. The output is a new 
+# anchor cluster file that can be used in downstream analyses.
+
+## import packages --------
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(optparse))
+
+## parse arguments --------
+# define command line arguments
+option_list <- list(
+  make_option(c("-i", "--input_anchor_clusters"), "Input anchor clusters", type="character"),
+  make_option(c("-s", "--splash_stats"), "SPLASH stats file", type="character"),
+  make_option(c("-o", "--output"), "Output file", type="character"),
+  make_option(c("--temp_dir"), "Temporary directory to store intermediate files", 
+              type="character")
+)
+
+# parse command line arguments
+opt <- parse_args(OptionParser(option_list = option_list))
+
+# check that input file exists
+if (!file.exists(opt$input_anchor_clusters) | !file.exists(opt$splash_stats) | is.null(opt$output)) {
+  stop("Must provide input and output files")
+}
+
+# create a temporary directory to store intermediate files
+if (!is.null(opt$temp_dir)) {
+  temp_dir <- ifelse(grepl("/$", opt$temp_dir),
+                     opt$temp_dir, 
+                     paste0(opt$temp_dir, "/"))
+  system(paste("mkdir -p", temp_dir))
+} else {
+  temp_dir <- file.path(dirname(opt$output_prefix), "tmp/")
+  system(paste("mkdir -p", temp_dir))
+}
+
+# cat to screen the input files, output files, temp dir and paramaters
+cat("\n###################################################################\n")
+cat("Running reorder_anchor_clusters.R with the following parameters:\n")
+cat(paste("Input clusters:", opt$input_anchor_clusters))
+cat("\n")
+cat(paste("SPLASH stats file:", opt$splash_stats))
+cat("\n")
+cat(paste("Output file:", opt$output))
+cat("\n")
+cat(paste("Temporary directory:", temp_dir))
+cat("\n")
+cat("###################################################################\n\n")
+
+# read in the anchor cluster file
+cat("Reading in the anchor clusters file\n")
+anchor_clusters <- fread(opt$input_anchor_clusters, 
+                    header = F, col.names = c("cluster_id", "anchor"))
+
+# read in the splash stats file (grepping for the anchors in the anchor file)
+cat("Reading in the splash stats file\n")
+## load the data
+# read in the headers of the input file to identify the effect_size_bin column
+headers <- fread(opt$splash_stats, nrows = 1, header=T)
+effect_size_bin_col <- grep("effect_size_bin", names(headers))
+
+# load the input file using awk to filter out rows with effect size < 0.7
+EFFECT_SIZE_CUTOFF = 0.6
+load_cmd <- paste0("cat ", opt$splash_stats, " | awk '{OFS=\"\t\"}{if ($", effect_size_bin_col, " >= ", EFFECT_SIZE_CUTOFF, ") print $0}'")
+if (grepl(".gz$", opt$input)) {
+  load_cmd = paste0("z", load_cmd)
+}
+splash_stats <- fread(cmd=load_cmd, header = T, select = 1:18)
+splash_stats <- splash_stats %>% filter(anchor %in% anchor_clusters$anchor)
+
+# join the splash stats file with the anchor clusters file
+cat("Joining the splash stats file with the anchor clusters file\n")
+anchor_clusters_with_stats <- anchor_clusters %>%
+  left_join(splash_stats, by = "anchor") %>% select(cluster_id, anchor, effect_size_bin, number_nonzero_samples)
+
+# reorder the cluster ids based on the mean effect size and number of nonzero samples
+cat("Reordering the cluster ids based on the mean effect size and number of nonzero samples\n")
+anchor_clusters_with_stats <- anchor_clusters_with_stats %>%
+  group_by(cluster_id) %>%
+  mutate(mean_effect_size = mean(effect_size_bin),
+         mean_nonzero_samples = mean(number_nonzero_samples)) %>%
+  mutate(sort_val = mean_effect_size * mean_nonzero_samples) %>%
+  arrange(desc(sort_val), cluster_id)  %>%
+  ungroup() %>% 
+  mutate(new_cluster_id = as.numeric(factor(cluster_id, levels = unique(cluster_id)))) %>% 
+  ungroup() %>% group_by(new_cluster_id) %>%
+  arrange(new_cluster_id, desc(effect_size_bin)) %>% # changed from desc(effect_size_bin * number_nonzero_samples)
+  ungroup() %>% select(new_cluster_id, anchor)
+
+# keep only one anchor per cluster (the one with the highest effect size)
+cat("Keeping only one anchor per cluster\n")
+anchor_clusters_with_stats <- anchor_clusters_with_stats %>%
+  group_by(new_cluster_id) %>%
+  slice(1) %>%
+  ungroup()
+
+# write out the new anchor clusters file
+cat("Writing out the new anchor clusters file\n")
+write_tsv(anchor_clusters_with_stats, opt$output, col_names = F, quote="none")
+
+cat("Done!\n")
+
+
