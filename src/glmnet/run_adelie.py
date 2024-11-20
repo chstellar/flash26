4b012126393cdc4f9f0dc13c9015583ac809eaa7
djcotter <dcotter1@stanford.edu>
2024-11-20 09:46:44 -0800

    test outout coef formatting for adelie script

--- src/glmnet/run_adelie.py
+++ src/glmnet/run_adelie.py
@@ -142,8 +142,8 @@ def main():
             
             # gather the coefficients and categorie names into two columns grouping by feature
             model_features = model_features.groupby("feature").agg({"coefficient": list, "category": list}).reset_index()
-            model_features["classes"] = model_features["category"].apply(lambda x: "[" + ",".join(str(x)) + "]")
-            model_features["coefficients"] = model_features["coefficient"].apply(lambda x: "[" + ",".join(str(x)) + "]")
+            model_features["classes"] = model_features["category"].apply(lambda x: "[" + ",".join(map(str, x)) + "]")
+            model_features["coefficients"] = model_features["coefficient"].apply(lambda x: "[" + ",".join(map(str, x)) + "]")
 
             # add column for metadata category
             model_features["metadata_category"] = metadata_col
