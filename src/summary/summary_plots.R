99d523958749b4ae5fce56f29de1d4e5a0344e58
Daniel Cotter <dcotter1@stanford.edu>
2024-11-21 09:46:43 -0800

    update summary and workflow scripts

--- src/summary/summary_plots.R
+++ src/summary/summary_plots.R
@@ -71,10 +71,14 @@ ggsave(opt$output, height=ifelse(num_metadata<10, 10, 2*num_metadata), width=10,
 
 
 ## plot all data in one plot
-load_cmd <- paste0("grep cluster ", "results/*/filter*/*/*/*malized/*_nonzero_coefficients.tsv")
-data1 <- fread(cmd=load_cmd, header=FALSE, sep="\t", 
+load_cmd1a <- paste0("grep cluster ", "results/*/filter*/*/*/*malized/*adelie*_nonzero_coefficients.tsv")
+data1a <- fread(cmd=load_cmd1a, header=FALSE, sep="\t", 
               col.names = c("path_metadata", "feature", "accuracy",
                             "sensitivity", "specificity", "classes", "coefficients"))
+load_cmd1b <- paste0("grep cluster ", "results/*/filter*/*/*/*malized/*glmnet*_nonzero_coefficients.tsv")
+data1b <- fread(cmd=load_cmd1b, header=FALSE, sep="\t", 
+                col.names = c("path_metadata", "feature", "accuracy",
+                              "sensitivity", "specificity", "classes", "coefficients"))
 
 load_cmd2 <- paste0("grep cluster ", "results/*/filter*/*/*/*_nonzero_coefficients.tsv")
 data2 <- fread(cmd=load_cmd2, header=FALSE, sep="\t", 
@@ -85,6 +89,8 @@ load_cmd3 <- paste0("grep cluster ", "results/*/filter*/*/*/*malized/*_important
 data3 <- fread(cmd=load_cmd3, header=FALSE, sep="\t", 
                col.names = c("path_metadata", "feature", "accuracy",
                              "sensitivity", "specificity", "Gini"))
+
+data1 <- data1a
 data <- rbind(data1,data2)
 data <- data %>% 
   distinct(path_metadata, .keep_all = T) %>%
@@ -150,22 +156,22 @@ dev.off()
 write_tsv(data, "all_summary_data_with_randForests.tsv", col_names = T, quote="needed")
 
 DATASET="eFaecium-CollEtAl"
-#DATASET="eColi-arcadia-amr"
+DATASET="eColi-arcadia-amr"
 plot_data <- data %>% 
   filter(dataset==DATASET) %>% 
-  select(filter, cluster_approach, metadata, model, sensitivity, num_clusters) %>% 
+  select(filter, cluster_approach, metadata, model, accuracy, num_clusters) %>% 
   distinct(filter, cluster_approach, metadata, model, .keep_all=T) %>% 
   mutate(model = paste0(model, " (", num_clusters, " clusters)")) %>% select(-num_clusters) %>% 
-  pivot_wider(names_from = model, values_from = sensitivity)
+  pivot_wider(names_from = model, values_from = accuracy)
 all_cols <- colnames(plot_data %>% select(-c(filter, cluster_approach, metadata, `ohe (50000 clusters)`)))
-pdf("results/summary_plots/eFaecium_sensitivity_comparison_241119.pdf")
+pdf("results/summary_plots/eColi_accuracy_comparison_using_adelie_results_241121.pdf")
 for (i in all_cols) {
   y_col = sym(i)
   p <- plot_data %>% 
-    ggplot(aes(x=`ohe (50000 clusters)`,y=!!y_col)) + geom_abline(slope=1, intercept = 0, color="grey", lty="dashed") + 
+    ggplot(aes(x=`ohe (50000 clusters)`,y=!!y_col)) + geom_abline(slope=1, intercept = 0, color="black", lty="dashed") + 
     geom_point(aes(shape=filter, color=cluster_approach), size=2) + theme_minimal() + coord_cartesian(ylim=c(0.4,1), xlim=c(0.4,1)) +
     ggrepel::geom_text_repel(aes(label=metadata)) +
-    ggtitle("Sensitivity")
+    ggtitle("Accuracy")
   print(p)
 }
 dev.off()
@@ -222,3 +228,71 @@ data %>% filter(accuracy>0.75) %>% filter(dataset=="eFaecium-CollEtAl") %>%
   mutate(seqs = sapply(str_extract_all(anno, "[ACTGN]{54}"), toString)) %>% 
   mutate(anno = sapply(str_extract_all(anno, "antibiotic|IS|ICE|ice|mge|MGE|Antibiotic"), \(x) toString(unique(x)))) %>% 
   select(seqs, anno) %>% separate_longer_delim(seqs, delim=", ") %>% write_tsv("logs/summary_annos.tsv", quote="needed", col_names=T)
+
+#######
+load_cmd1a <- paste0("grep cluster ", "results/*/filter*/*/*/*malized/*adelie*_nonzero_coefficients.tsv")
+data1a <- fread(cmd=load_cmd1a, header=FALSE, sep="\t", 
+                col.names = c("path_metadata", "feature", "accuracy",
+                              "sensitivity", "specificity", "classes", "coefficients"))
+load_cmd1b <- paste0("grep cluster ", "results/*/filter*/*/*/*malized/*glmnet*_nonzero_coefficients.tsv")
+data1b <- fread(cmd=load_cmd1b, header=FALSE, sep="\t", 
+                col.names = c("path_metadata", "feature", "accuracy",
+                              "sensitivity", "specificity", "classes", "coefficients"))
+
+data1a$measure <- "adelie"
+data1b$measure <- "glmnet"
+data<- rbind(data1a,data1b)
+data <- data %>% 
+  distinct(path_metadata, .keep_all = T) %>%
+  separate(path_metadata, into=c("path", "metadata"), sep=":") %>%
+  select(path, metadata, accuracy, sensitivity, specificity, measure)
+
+data <- data %>% mutate(num_clusters = str_extract(path, "top(\\d+)", group=1))
+data <- data %>% mutate(path=dirname(gsub("^results/", "", path))) %>%
+  mutate(path=gsub("/","_",path)) %>%
+  dplyr::rename(paramater_set=path)
+data <- data %>% mutate(paramater_set=str_replace(paramater_set, "_", "/")) %>% 
+  separate(paramater_set, into=c("dataset", "paramater_set"), sep="/")
+data <- data %>% mutate(model=str_extract(paramater_set, 
+                                          'hyenaMarlowe_normalized|hyenaMarlowe_unnormalized|esm_normalized|esm_unnormalized|hyena_normalized|hyena_unnormalized|ohe')) %>% arrange(desc(model))
+
+data <- data %>% mutate(filter = str_extract(paramater_set, "(filter\\d)_",group=1)) %>% 
+  mutate(cluster_approach = str_extract(paramater_set, "filter\\d_([A-Za-z-]+)_", group=1))
+
+data <- data %>% filter(as.integer(num_clusters)>=10000)
+DATASET="eFaecium-CollEtAl"
+#DATASET="eColi-arcadia-amr"
+plot_data <- data %>% 
+  filter(dataset==DATASET) %>% 
+  select(filter, cluster_approach, metadata, model, specificity, num_clusters, measure) %>% 
+  pivot_wider(names_from = measure, values_from = specificity)
+
+models <- plot_data %>% filter(!is.na(adelie)) %>% filter(!is.na(glmnet)) %>% pull(model) %>% unique()
+
+pdf("results/summary_plots/eFaecium_specificity_adelie_vs_glmnet_comparison_241120.pdf")
+for (i in models) {
+  p <- plot_data %>% filter(model==i) %>%
+    ggplot(aes(x=glmnet,y=adelie)) + geom_abline(slope=1, intercept = 0, color="black", lty="dashed") + 
+    geom_point(aes(shape=filter, color=cluster_approach), size=2) + theme_minimal() + coord_cartesian(ylim=c(0.4,1), xlim=c(0.4,1)) +
+    ggrepel::geom_text_repel(aes(label=metadata)) +
+    ggtitle(paste("Specificity (10k clusters):", i))
+  print(p)
+}
+dev.off()
+
+
+## compare e faecium results to the lancet 
+lancet_data <- fread("/oak/stanford/groups/horence/dcotter1/utility_files/lancet_RIS_predictions.csv")
+lancet_data <- lancet_data %>% mutate(metadata_category=paste0(str_to_lower(metadata_category), "_RIS")) %>% rename(metadata=metadata_category)
+comp_data <-data %>% filter(dataset =="eFaecium-CollEtAl") %>% left_join(lancet_data)
+
+comp_data %>% ggplot(aes(x=lancet_accuracy, y=accuracy)) + geom_abline(intercept=0, slope=1, color="black", lty="dashed") +
+  geom_point(aes(color=filter, shape=model)) + theme_minimal() +
+  coord_cartesian(xlim=c(0.6,1), ylim=c(0.6,1)) + ggtitle("Accuracy") + geom_text(y=0.95,aes(x=lancet_accuracy, label=metadata), angle=90, size=3)
+comp_data %>% ggplot(aes(x=lancet_sensitivity, y=sensitivity)) + geom_abline(intercept=0, slope=1, color="black", lty="dashed") +
+  geom_point(aes(color=filter, shape=model)) + theme_minimal() +
+  coord_cartesian(xlim=c(0.6,1), ylim=c(0.6,1)) + ggtitle("Sensitivity") + geom_text(y=0.95,aes(x=lancet_sensitivity, label=metadata), angle=90, size=3)
+comp_data %>% ggplot(aes(x=lancet_specificity, y=specificity)) + geom_abline(intercept=0, slope=1, color="black", lty="dashed") +
+  geom_point(aes(color=filter, shape=model)) + theme_minimal() +
+  coord_cartesian(xlim=c(0.6,1), ylim=c(0.6,1)) + ggtitle("Specificity") + geom_text(y=0.95,aes(x=lancet_specificity, label=metadata), angle=90, size=3)
+
