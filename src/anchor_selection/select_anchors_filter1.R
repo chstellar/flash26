625b589d3648c4e5d63ac0ef0294fcb4514164d8
djcotter <dcotter1@stanford.edu>
2026-01-26 12:23:56 -0800

    clean up code and syntax for anchor filtering script

--- src/anchor_selection/select_anchors_filter1.R
+++ src/anchor_selection/select_anchors_filter1.R
@@ -12,6 +12,8 @@
 # Filter 1: number of nonzero samples > 10th percentile
 # Filter 1: no lookup table hits to artifacts
 # Filter 1: select top 1,000,000 anchors by number of nonzero samples
+# These numbers can be changed via command line arguments
+# and by changing the varialbes in the config file for the pipeline.
 
 ## import packages --------
 suppressPackageStartupMessages(library(data.table))
@@ -23,18 +25,23 @@ suppressPackageStartupMessages(library(Biostrings))
 # define command line arguments
 option_list <- list(
   make_option(c("-i", "--input"), "Input file", type = "character"),
-  make_option(c("-o", "--output"), "Output file", type ="character"),
+  make_option(c("-o", "--output"), "Output file", type = "character"),
   make_option(c("-n", "--num_anchors"), "Number of anchors to select",
-              type = "integer", default = 1000000),
+    type = "integer", default = 1000000
+  ),
   make_option(c("-e", "--effect_size"), "Effect size threshold",
-              type = "numeric", default = 0.6),
+    type = "numeric", default = 0.6
+  ),
   make_option(c("-l", "--lookup_table"), "Lookup table file",
-              type = "character"),
+    type = "character"
+  ),
   make_option(c("--splash_bin"), "Path to SPLASH binary folder",
-              type = "character", default="splash-2.11.6/"),
+    type = "character", default = "splash-2.11.6/"
+  ),
   make_option(c("--temp_dir"),
-              "Temporary directory to store intermediate files",
-              type = "character")
+    "Temporary directory to store intermediate files",
+    type = "character"
+  )
 )
 
 # parse command line arguments
@@ -54,8 +61,9 @@ anchors_only_out <- opt$output
 # create a temporary directory to store intermediate files
 if (!is.null(opt$temp_dir)) {
   temp_dir <- ifelse(grepl("/$", opt$temp_dir),
-                     opt$temp_dir, 
-                     paste0(opt$temp_dir, "/"))
+    opt$temp_dir,
+    paste0(opt$temp_dir, "/")
+  )
   system(paste("mkdir -p", temp_dir))
 } else {
   temp_dir <- file.path(dirname(opt$output), "tmp/")
@@ -97,24 +105,31 @@ nonzero_samples_col <- grep("number_nonzero_samples", names(headers))
 max_col_to_read <- max(effect_size_bin_col, nonzero_samples_col) + 1
 
 # load the input file using awk to filter out rows with effect size < 0.7
-EFFECT_SIZE_CUTOFF = opt$effect_size
+EFFECT_SIZE_CUTOFF <- opt$effect_size
 
-load_cmd <- paste0("cut -f1-", max_col_to_read, " ",
-                   opt$input, " | awk '{OFS=\"\t\"}{if ($",
-                   effect_size_bin_col, " >= ", EFFECT_SIZE_CUTOFF,
-                   ") print $0}'")
+load_cmd <- paste0(
+  "cat ", opt$input, " | ",
+  "cut -f1-", max_col_to_read,
+  " | awk '{OFS=\"\t\"}{if ($",
+  effect_size_bin_col, " >= ", EFFECT_SIZE_CUTOFF,
+  ") print $0}'"
+)
 if (grepl(".gz$", opt$input)) {
   load_cmd <- paste0("z", load_cmd)
 }
 
 # only select the columns up to number_nonzero_samples
-cat(paste("Reading in anchors from",
-          opt$input, "with effect size >=",
-          opt$effect_size))
+cat(paste(
+  "Reading in anchors from",
+  opt$input, "with effect size >=",
+  opt$effect_size
+))
 cat("\n\n")
 dt <- fread(cmd = load_cmd, col.names = names(headers)[1:max_col_to_read]) %>%
-  mutate(effect_size_bin = as.numeric(effect_size_bin),
-         number_nonzero_samples = as.numeric(number_nonzero_samples))
+  mutate(
+    effect_size_bin = as.numeric(effect_size_bin),
+    number_nonzero_samples = as.numeric(number_nonzero_samples)
+  )
 
 # filter out the bottom 10% of anchors by number nonzero samples
 sample_cutoff <- quantile(dt$number_nonzero_samples, 0.1)
@@ -122,10 +137,12 @@ dt <- dt %>% filter(number_nonzero_samples >= sample_cutoff)
 
 ## run lookup table to filter out artifacts -----------
 # write all anchors to a file
-anchors_to_keep <- dt %>% select(anchor) %>% distinct() 
+anchors_to_keep <- dt %>%
+  select(anchor) %>%
+  distinct()
 anchor_file <- file.path(temp_dir, "all_anchors.txt") %>% gsub("//", "/", .)
 anchors_to_keep %>%
-  write.table(anchor_file, row.names=FALSE, col.names=FALSE, quote=FALSE)
+  write.table(anchor_file, row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 # write anchor to a fasta file as well (for lookuptable)
 anchors_fasta_file <- file.path(temp_dir, "all_anchors.fasta") %>%
@@ -137,11 +154,13 @@ writeXStringSet(anchors_dna, anchors_fasta_file)
 # run lookup table
 out_lookup_stats <- file.path(temp_dir, "lookup_stats.txt") %>%
   gsub("//", "/", .)
-lookup_cmd <- paste0(file.path(opt$splash_bin, "lookup_table"),
-                     " query --kmer_skip 1 ",
-                     "--truncate_paths --stats_fmt with_stats ",
-                     anchors_fasta_file,
-                     " ", opt$lookup_table, " ", out_lookup_stats)
+lookup_cmd <- paste0(
+  file.path(opt$splash_bin, "lookup_table"),
+  " query --kmer_skip 1 ",
+  "--truncate_paths --stats_fmt with_stats ",
+  anchors_fasta_file,
+  " ", opt$lookup_table, " ", out_lookup_stats
+)
 system(paste("rm", out_lookup_stats))
 if (file.exists(out_lookup_stats)) {
   cat("Lookup stats already in tmp directory. Delete tmp to force run again.\n")
@@ -152,56 +171,68 @@ if (file.exists(out_lookup_stats)) {
   cat("Finished Querying lookup table. Filtering anchors for artifacts...\n\n")
 }
 
-result <- tryCatch({
-  # Read in the lookup table stats
-  lookup_stats <- fread(out_lookup_stats,
-                        header = FALSE,
-                        col.names = c("query", "stats"), sep = "\t")
-  lookup_stats <- lookup_stats %>% mutate(anchor = anchors_to_keep$anchor)
-  # Filter out anchors that have lookup table hits to artifacts
-  artifact_pattern <- paste0("plas|illum|syn|arp|RF|JUNK|",
-                             "Ral|purge|P,|Univec|cattle|chicken")
-  anchors_to_keep <- lookup_stats %>%
-    filter(!grepl(artifact_pattern, query, ignore.case = TRUE)) %>%
-    select(anchor)
-  # Continue with the rest of your code if no error occurs
-  anchors_to_keep
+result <- tryCatch(
+  {
+    # Read in the lookup table stats
+    lookup_stats <- fread(out_lookup_stats,
+      header = FALSE,
+      col.names = c("query", "stats"), sep = "\t"
+    )
+    lookup_stats <- lookup_stats %>% mutate(anchor = anchors_to_keep$anchor)
+    # Filter out anchors that have lookup table hits to artifacts
+    artifact_pattern <- paste0(
+      "plas|illum|syn|arp|RF|JUNK|",
+      "Ral|purge|P,|Univec|cattle|chicken"
+    )
+    anchors_to_keep <- lookup_stats %>%
+      filter(!grepl(artifact_pattern, query, ignore.case = TRUE)) %>%
+      select(anchor)
+    # Continue with the rest of your code if no error occurs
+    anchors_to_keep
 
-  cat(paste0("Finished filtering. Kept ",
-             nrow(anchors_to_keep),
-             " anchors out of ",
-             nrow(lookup_stats),
-             " total anchors.\n"))
-  cat(paste0("Keeping the top ",
-             opt$num_anchors,
-             " by number_nonzero_samples for further analysis...\n\n"))
+    cat(paste0(
+      "Finished filtering. Kept ",
+      nrow(anchors_to_keep),
+      " anchors out of ",
+      nrow(lookup_stats),
+      " total anchors.\n"
+    ))
+    cat(paste0(
+      "Keeping the top ",
+      opt$num_anchors,
+      " by number_nonzero_samples for further analysis...\n\n"
+    ))
+  },
+  error = function(e) {
+    message("An error occurred: ", e$message)
+    # Check the length of anchors_to_keep
+    if (exists("anchors_to_keep") && nchar(anchors_to_keep[1, ]) < 11) {
+      message("Continuing execution without modifying anchors_to_keep.")
+      # Continue with the rest of your code
+      # For example, you can return anchors_to_keep or perform other operations
+      return(anchors_to_keep)
+    } else {
+      message("Stopping execution as anchors_to_keep has 11 or more elements.")
+      stop(e) # Stop execution and raise the error
+    }
 
-}, error = function(e) {
-  message("An error occurred: ", e$message)
-  # Check the length of anchors_to_keep
-  if (exists("anchors_to_keep") && nchar(anchors_to_keep[1,]) < 11) {
-    message("Continuing execution without modifying anchors_to_keep.")
-    # Continue with the rest of your code
-    # For example, you can return anchors_to_keep or perform other operations
-    return(anchors_to_keep)
-  } else {
-    message("Stopping execution as anchors_to_keep has 11 or more elements.")
-    stop(e)  # Stop execution and raise the error
+
+    cat(paste0(
+      "Finished filtering. ",
+      "Lookup table failed likely due to short anchors. ",
+      "Kept all anchors.\n"
+    ))
+    cat(paste0(
+      "Keeping the top ",
+      opt$num_anchors,
+      " by number_nonzero_samples for further analysis...\n\n"
+    ))
   }
-
-
-  cat(paste0("Finished filtering. ",
-             "Lookup table failed likely due to short anchors. ",
-             "Kept all anchors.\n"))
-  cat(paste0("Keeping the top ",
-             opt$num_anchors,
-             " by number_nonzero_samples for further analysis...\n\n"))
-
-})
+)
 
 ## select the most important anchors -----------
-anchors_to_keep <- anchors_to_keep %>% 
-  left_join(dt %>% select(anchor, number_nonzero_samples), by="anchor") %>%
+anchors_to_keep <- anchors_to_keep %>%
+  left_join(dt %>% select(anchor, number_nonzero_samples), by = "anchor") %>%
   arrange(desc(number_nonzero_samples)) %>%
   head(opt$num_anchors)
 
@@ -209,5 +240,7 @@ anchors_to_keep <- anchors_to_keep %>%
 cat(paste("Writing", nrow(anchors_to_keep), "anchors to", anchors_only_out))
 cat("\n")
 anchors_to_keep$anchor %>%
-  write.table(anchors_only_out, row.names = FALSE,
-              col.names = FALSE, quote = FALSE)
+  write.table(anchors_only_out,
+    row.names = FALSE,
+    col.names = FALSE, quote = FALSE
+  )
