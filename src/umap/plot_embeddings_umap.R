b6e80e43e4bccc74fb161f9417e5f72f5f62339e
djcotter <dcotter1@stanford.edu>
2026-03-23 09:01:45 -0700

    fix syntax error in umap/tsne plotting script

--- src/umap/plot_embeddings_umap.R
+++ src/umap/plot_embeddings_umap.R
@@ -1,11 +1,11 @@
-SuppressPackageStartupMessages(library(data.table))
-SuppressPackageStartupMessages(library(optparse))
-SuppressPackageStartupMessages(library(tidyverse))
-SuppressPackageStartupMessages(library(umap))
-SuppressPackageStartupMessages(library(ggtext))
-SuppressPackageStartupMessages(library(feather))
-SuppressPackageStartupMessages(library(fossil))
-SuppressPackageStartupMessages(library(dbscan))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(umap))
+suppressPackageStartupMessages(library(ggtext))
+suppressPackageStartupMessages(library(feather))
+suppressPackageStartupMessages(library(fossil))
+suppressPackageStartupMessages(library(dbscan))
 
 option_list <- list(
   make_option(
@@ -16,7 +16,7 @@ option_list <- list(
     metavar = "character"
   ),
   make_option(
-    c("-f", "--feather"),
+    c("-f", "--embeddings"),
     type = "character",
     default = NULL,
     help = "Path to feather file",
@@ -30,7 +30,7 @@ option_list <- list(
     metavar = "character"
   ),
   make_option(
-    c("-n", "--num_pcs"),
+    c("-n", "--num_PCs"),
     type = "integer",
     default = 10,
     help = "Number of principal components for UMAP",
@@ -48,7 +48,7 @@ option_list <- list(
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
-if (is.null(opt$metadata) || is.null(opt$feather)) {
+if (is.null(opt$metadata) || is.null(opt$embeddings)) {
   stop("Both metadata and feather file paths must be provided.")
 }
 
@@ -57,13 +57,14 @@ if (is.null(opt$output)) {
 }
 
 metadata_path <- opt$metadata
-feather_path <- opt$feather
-num_pcs <- opt$num_pcs
+feather_path <- opt$embeddings
+num_pcs <- opt$num_PCs
 
 # Load metadata
 metadata <- fread(metadata_path,
-                  header = TRUE,
-                  stringsAsFactors = FALSE) %>%
+  header = TRUE,
+  stringsAsFactors = FALSE
+) %>%
   mutate(sample_name = as.character(sample_name)) %>%
   select(sample_name, everything()) # Ensure sample_name is the first column
 
@@ -98,19 +99,21 @@ if (nrow(pca$rotation) > 15) {
     ungroup() %>%
     select(PC1:!!as.name(paste0("PC", n))) %>%
     umap(config = custom.settings)
-  
+
   layout <- umap_res$layout %>%
     as.data.frame() %>%
     rename(UMAP1 = V1, UMAP2 = V2)
   # use hdbscan to cluster the UMAP results
-  hdbscan_result <- hdbscan(layout[, c("UMAP1", "UMAP2")], minPts = 5)  # Adjust minPts as needed
+  hdbscan_result <- hdbscan(layout[, c("UMAP1", "UMAP2")], minPts = 5) # Adjust minPts as needed
   layout$Cluster <- hdbscan_result$cluster
-  
-  umap_df <- pca_sub %>% select(-starts_with("PC")) %>% cbind(layout)
-  
+
+  umap_df <- pca_sub %>%
+    select(-starts_with("PC")) %>%
+    cbind(layout)
+
   if (is.na(opt$metadata_column)) {
     my_metadata <- "HDBSCAN_Cluster"
-    umap_df <- umap_df %>% 
+    umap_df <- umap_df %>%
       mutate(Cluster = as.factor(Cluster)) %>%
       select(sample_name, UMAP1, UMAP2, Cluster) %>%
       rename(!!as.name(my_metadata) := Cluster)
@@ -120,28 +123,29 @@ if (nrow(pca$rotation) > 15) {
     umap_df <- umap_df %>%
       left_join(metadata, by = "sample_name") %>%
       select(sample_name, UMAP1, UMAP2, Cluster, !!as.name(my_metadata))
-    
+
     # calculate the random index between the metadata and the clusters
     rand_idx <- round(fossil::rand.index(as.numeric(factor(umap_df[[my_metadata]])), umap_df$Cluster), 3)
   }
-  
+
   # label the top 8 most abundant classes and change the rest to "other"
   classes <- umap_df %>%
     group_by(!!as.name(my_metadata)) %>%
     summarise(Count = n()) %>%
     top_n(8, Count) %>%
     pull(!!as.name(my_metadata))
-  
+
   umap_df <- umap_df %>%
     mutate(class = ifelse(
-      !!as.name(my_metadata) %in% classes,!!as.name(my_metadata),
+      !!as.name(my_metadata) %in% classes, !!as.name(my_metadata),
       NA
     )) %>%
     mutate(class = factor(class, levels = c(classes, NA)))
-  
+
   p <- umap_df %>%
     ggplot(aes(x = UMAP1, y = UMAP2, color = class)) +
-    xlab("UMAP 1") + ylab("UMAP 2") +
+    xlab("UMAP 1") +
+    ylab("UMAP 2") +
     geom_point(alpha = 0.8, size = 3) +
     scale_color_brewer(
       paste(my_metadata),
@@ -158,7 +162,6 @@ if (nrow(pca$rotation) > 15) {
       subtitle = paste("rand index:", rand_idx, "\nN_PCs:", n)
     ) +
     theme_minimal()
-  
 } else {
   stop("Not enough PCs to run UMAP. Please increase the number of PCs or check your data.")
 }
