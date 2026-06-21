8b8f7904258bf15857c04b82b9b16668b4d3c534
chstellar <v8514616@outlook.com>
2026-06-21 16:31:21 -0700

    bash

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=2-00:00:00 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=1G
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=chesteryu@stanford.edu
 
@@ -12,7 +12,11 @@ eval "$(/oak/stanford/groups/horence/chester/dabs_ref/miniforge3/bin/conda shell
 eval "$(mamba shell hook --shell bash)"
 mamba activate flash
 
-# NUM_CORES=${SLURM_CPUS_PER_TASK:-1}
+# snakemake --unlock -s Snakefile
+# snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s Snakefile
 
-snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings
+snakemake --unlock -s Snakefile26062100
+snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s Snakefile26062100
+
+# NUM_CORES=${SLURM_CPUS_PER_TASK:-1}
 # snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ -j $NUM_CORES all_embeddings
