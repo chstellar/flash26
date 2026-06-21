d217ba00b1659dc99fd43a4eafc042913e4e8f3d
chstellar <v8514616@outlook.com>
2026-06-21 11:41:35 -0700

    bsah

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 #SBATCH -p horence
-#SBATCH --time=0-12:00:00 
+#SBATCH --time=2-00:00:00 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=chesteryu@stanford.edu
 
@@ -12,7 +12,7 @@ eval "$(/oak/stanford/groups/horence/chester/dabs_ref/miniforge3/bin/conda shell
 eval "$(mamba shell hook --shell bash)"
 mamba activate flash
 
-NUM_CORES=${SLURM_CPUS_PER_TASK:-1}
+# NUM_CORES=${SLURM_CPUS_PER_TASK:-1}
 
-snakemake --sdm conda -j $NUM_CORES all_embeddings
-# snakemake all_embeddings
+snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings
+# snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ -j $NUM_CORES all_embeddings
