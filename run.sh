deba64f12fbbdc9cc5a57a80f7550925fbb1bc9a
chstellar <v8514616@outlook.com>
2026-06-20 20:10:51 -0700

    first commit

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH -p horence
+#SBATCH --time=0-12:00:00 
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=chesteryu@stanford.edu
+
+eval "$(/oak/stanford/groups/horence/chester/dabs_ref/miniforge3/bin/conda shell.bash hook)" 
+eval "$(mamba shell hook --shell bash)"
+mamba activate snakemake
+
+NUM_CORES=${SLURM_CPUS_PER_TASK:-1}
+
+snakemake --sdm conda -j $NUM_CORES --detailed-summary  all_embeddings
+# snakemake all_embeddings
