7510ecff8a289bd04065724067d75427ce1977bb
chstellar <v8514616@outlook.com>
2026-06-22 09:37:27 -0700

    bash

--- run.sh
+++ run.sh
@@ -13,11 +13,11 @@ eval "$(/oak/stanford/groups/horence/chester/dabs_ref/miniforge3/bin/conda shell
 eval "$(mamba shell hook --shell bash)"
 mamba activate flash
 
-snakemake --unlock -s 260620_00
-snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s 260620_00
+# SNAKEMAKE_FILE="${1:-260620_00}"
+SNAKEMAKE_FILE="${1:-260621_00}"
 
-# snakemake --unlock -s Snakefile26062100
-# snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s Snakefile26062100
+snakemake --unlock -s $SNAKEMAKE_FILE
+snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s $SNAKEMAKE_FILE
 
 # NUM_CORES=${SLURM_CPUS_PER_TASK:-1}
 # snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ -j $NUM_CORES all_embeddings
