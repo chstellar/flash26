13ab367bc8c563be3f29ec885a98788275776c49
chstellar <v8514616@outlook.com>
2026-06-21 20:08:22 -0700

    bash

--- run.sh
+++ run.sh
@@ -8,15 +8,16 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=chesteryu@stanford.edu
 
+ml purge
 eval "$(/oak/stanford/groups/horence/chester/dabs_ref/miniforge3/bin/conda shell.bash hook)" 
 eval "$(mamba shell hook --shell bash)"
 mamba activate flash
 
-# snakemake --unlock -s Snakefile
-# snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s Snakefile
+snakemake --unlock -s 260620_00
+snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s 260620_00
 
-snakemake --unlock -s Snakefile26062100
-snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s Snakefile26062100
+# snakemake --unlock -s Snakefile26062100
+# snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ all_embeddings -s Snakefile26062100
 
 # NUM_CORES=${SLURM_CPUS_PER_TASK:-1}
 # snakemake --sdm conda --use-conda --conda-base-path /oak/stanford/groups/horence/chester/dabs_ref/miniforge3 --profile slurm_profile/ -j $NUM_CORES all_embeddings
